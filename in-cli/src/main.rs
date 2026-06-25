0d81f9dea17f241cb9b1f213dd4c9ca2d4b421c3
Max Carter 祁明思 <undivisible@vk.com>
2026-06-25 12:24:35 +1000

    bump version to 0.6.11

--- in-cli/src/main.rs
+++ in-cli/src/main.rs
@@ -30,7 +30,7 @@ enum InError {
 
 #[derive(Parser, Debug)]
 #[command(name = "in")]
-#[command(version = "0.6.10")]
+#[command(version = "0.6.11")]
 #[command(about = "inauguration v0.5.1")]
 struct Cli {
     #[command(subcommand)]
