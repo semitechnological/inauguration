db14a30c913da0d4689b8351d86073f6011a7e8d
Max Carter 祁明思 <undivisible@vk.com>
2026-06-25 13:19:07 +1000

    bump version to 0.7.0

--- in-cli/src/main.rs
+++ in-cli/src/main.rs
@@ -30,7 +30,7 @@ enum InError {
 
 #[derive(Parser, Debug)]
 #[command(name = "in")]
-#[command(version = "0.6.14")]
+#[command(version = "0.7.0")]
 #[command(about = "inauguration v0.5.1")]
 struct Cli {
     #[command(subcommand)]
